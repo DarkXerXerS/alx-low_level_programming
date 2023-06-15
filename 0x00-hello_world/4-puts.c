87be196ee77d797ef5a7a25b46f33de31a6e6871
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-15 06:52:30 -0700

    modification

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/*
+ *main -Funtion
+ *return: 0(sucess)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

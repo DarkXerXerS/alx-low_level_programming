2ea4039a78a590189dab7b6d4c0ced651fde0eb8
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-15 07:01:23 -0700

    rjngRYUGFBRIUG

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
  *main -Funtion
+ *
+ *Description: This function is the entry point of the program
+ *
  *return: 0(sucess)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

c69f7de03f902219acc57928e994daacc80306bd
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-18 03:26:35 -0700

    uuywidvidvj

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 113 && i == 101)
+			continue;
+		putchar(i);
+	}
+	putchar('\n');

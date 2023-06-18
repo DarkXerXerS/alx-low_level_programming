a30f401af857cea5932aeb3c217c5adcb8be4219
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-18 03:06:53 -0700

    uuywidvidvj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print with putchar a to z
+ *
+ * Return: (sucess);
+ */
+int main(void)
+{
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

846e2dc01d7896100e57cd9765b00826f1ec548e
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-18 19:42:23 -0700

    iforgi

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: (sucess)
+ */
+int main(void)
+{
+	char i'
+
+	for (i = 'z'; i >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

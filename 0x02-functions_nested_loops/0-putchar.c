7e445328080b5014cb690a3c8269f0ca0ca5de3f
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-20 16:59:52 -0700

    modification

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i, size;
+	int str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
+	size = sizeof(str) / sizeof(int);
+	for (i = 0; i < size; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }

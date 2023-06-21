55ba901161ab4fe0052d46d1aa13953bbae9202b
DarkXerXerS <darkxerxers29@gmail.com>
2023-06-20 18:55:14 -0700

    modification

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print all the alphabet in lower case
+ *
+ * Return: no return (void)
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
